18310d7fd0c78df84f15f7809a5162cc0b74a2ab
Hlsorrells <hlsorrells.dev@gmail.com>
2022-02-20 11:22:55 -0500

    added a page not found route for url error handling

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<h1>404... Page Not Found</h1>} />
       </Routes>
     </>
   )
