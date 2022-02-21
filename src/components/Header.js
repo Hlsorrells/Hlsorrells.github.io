19a33e8d53b21fe57834fde855fec128beb5b54b
Hlsorrells <hlsorrells.dev@gmail.com>
2022-02-21 11:50:15 -0500

    added logo to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
+import logo from '../images/android-chrome-192x192.png';
 
 const Header = () => {
     return (
         <>
             <header>
                 <div className='container container-flex'>
-                    <nav style={{
-                        borderBottom: "solid 1px",
-                        paddingBottom: "1rem"
-                    }}>
-                        <Link to='/'>Home</Link> |{" "}
-                        <Link to='/about'>About</Link> |{" "}
-                        <Link to='/contact'>Contact</Link>
-                    </nav>
+                    <div className='container container-flex'>
+                        <div className="logoContainer">
+                            <img src={logo} alt='logo' className="logo" />
+                        </div>
+                        <nav style={{
+                            borderBottom: "solid 1px",
+                            paddingBottom: "1rem"
+                        }}>
+                            <Link to='/'>Home</Link> |{" "}
+                            <Link to='/about'>About</Link> |{" "}
+                            <Link to='/contact'>Contact</Link>
+                        </nav>
+                    </div>
                 </div>
             </header>
         </>
