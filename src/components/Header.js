24e93e6a34469682d3c6395ea4074e613b666511
Hlsorrells <hlsorrells.dev@gmail.com>
2022-02-21 20:29:26 -0500

    changed Link to NavLink and added useLocation to retrieve pathname to indicate active link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from 'react-router-dom';
 import logo from '../images/android-chrome-192x192.png';
+import './Header.css';
 
 const Header = () => {
+    // assigning location variable
+    const { pathname } = useLocation();
+
+    // JS split method to get the name of the path in array
+    const splitLocation = pathname.split('/');
+
     return (
         <>
             <header>
-                <div className='container container-flex'>
-                    <div className='container container-flex'>
-                        <div className="logoContainer">
-                            <img src={logo} alt='logo' className="logo" />
-                        </div>
-                        <nav style={{
-                            borderBottom: "solid 1px",
-                            paddingBottom: "1rem"
-                        }}>
-                            <Link to='/'>Home</Link> |{" "}
-                            <Link to='/about'>About</Link> |{" "}
-                            <Link to='/contact'>Contact</Link>
-                        </nav>
+                <div className='container'>
+                    <div className='logoContainer'>
+                        <img src={logo} alt='logo' className='logo' />
                     </div>
+                    <nav>
+                        {/* Check the current pathname using JS ternary operator & if true setting class to active*/}
+                        <NavLink className={splitLocation[1] === '' ? 'active' : ''} to='/' >Home</NavLink> {' '}
+                        <NavLink className={splitLocation[1] === 'about' ? 'active' : ''} to='/about' >About</NavLink> {' '}
+                        <NavLink className={splitLocation[1] === 'contact' ? 'active' : ''} to='/contact' >Contact</NavLink>
+                    </nav>
                 </div>
             </header>
         </>
