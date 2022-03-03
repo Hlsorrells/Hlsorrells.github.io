64f18c0aea5d82c27f0bc5c14c0754de9b11e3ea
Hlsorrells <hlsorrells.dev@gmail.com>
2022-03-02 19:47:38 -0500

    created component for contact information table

---
+++ src/components/ContactInfoTable.js
@@ -0,0 +1,26 @@
+const ContactInfoTable = () => {
+    return (
+        <>
+            <table >
+                <tr>
+                    <th>Email me here 📧</th>
+                    <td><a className='contactLink' href='mailto:hlsorrells.dev@gmail.com'>hlsorrells.dev@gmail.com</a></td>
+                </tr>
+                <tr>
+                    <th>Browse through my code repos 💻</th>
+                    <td><a className='contactLink' href='https://gidub.com/Hlsorrells'>GitHub profile</a></td>
+                </tr>
+                <tr>
+                    <th>Check out my skills and experience 👩‍💻</th>
+                    <td><a className='contactLink' href='https://www.linkedin.com/in/heather-sorrells/'>LinkedIn profile</a></td>
+                </tr>
+                <tr>
+                    <th>Read my resume here 👉🏼</th>
+                    <td><a className='contactLink' href='https://bit.ly/Hlsorrells_Resume'>My Resume</a></td>
+                </tr>
+            </table>
+        </>
+    )
+}
+
+export default ContactInfoTable;
