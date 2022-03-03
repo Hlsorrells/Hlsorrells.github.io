26e984d2a82874a453ff881ef71b1dd78cff177e
Hlsorrells <hlsorrells.dev@gmail.com>
2022-03-02 19:53:59 -0500

    updated and styled the contact page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,5 +1,37 @@
+import ContactInfoTable from '../components/ContactInfoTable';
+import profilePic from '../images/heatherSorrellsProfilePic.PNG';
+
 const Contact = () => {
-    return <h1>This is my Contact Page</h1>
+    return (
+        <>
+            <main className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <h1>Thanks so much for visiting my portfolio!</h1>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-2'></div>
+                    <div className='col-8'>
+                        <img className='round mx-auto d-block' src={profilePic} alt='Heather Sorrells' />
+                    </div>
+                    <div className='col-2'></div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <h3>Heather Sorrells</h3>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-2'></div>
+                    <div className='d-flex col-sm-8'>
+                        <ContactInfoTable />
+                    </div>
+                    <div className='col-2'></div>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default Contact;
