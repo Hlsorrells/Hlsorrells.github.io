986bfd2c1ff68a9a9880b6399659a96696698315
Hlsorrells <hlsorrells.dev@gmail.com>
2022-03-06 11:10:07 -0500

    created the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <h6>This site was made with a dash of challenge, ample fun, and lots of learning!</h6>
+        </footer>
+        </>
+    )
+};
+
+export default Footer;
